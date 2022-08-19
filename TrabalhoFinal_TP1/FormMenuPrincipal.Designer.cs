a7d325937699b88aecfea64bbe87a662e3bf8f68
Thiago Costa <thiago_291@live.com>
2022-08-19 11:52:09 -0300

    Criação do diretório do trabalho final de TP1.

---
+++ TrabalhoFinal_TP1/FormMenuPrincipal.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace TrabalhoFinal_TP1
+{
+    partial class FormMenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCliente = new System.Windows.Forms.Button();
+            this.btnCarros = new System.Windows.Forms.Button();
+            this.btnPedidos = new System.Windows.Forms.Button();
+            this.btnSobre = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCliente
+            // 
+            this.btnCliente.Location = new System.Drawing.Point(111, 264);
+            this.btnCliente.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnCliente.Name = "btnCliente";
+            this.btnCliente.Size = new System.Drawing.Size(365, 63);
+            this.btnCliente.TabIndex = 0;
+            this.btnCliente.Text = "Clientes";
+            this.btnCliente.UseVisualStyleBackColor = true;
+            // 
+            // btnCarros
+            // 
+            this.btnCarros.Location = new System.Drawing.Point(111, 366);
+            this.btnCarros.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnCarros.Name = "btnCarros";
+            this.btnCarros.Size = new System.Drawing.Size(365, 63);
+            this.btnCarros.TabIndex = 1;
+            this.btnCarros.Text = "Consulta de carros";
+            this.btnCarros.UseVisualStyleBackColor = true;
+            // 
+            // btnPedidos
+            // 
+            this.btnPedidos.Location = new System.Drawing.Point(111, 161);
+            this.btnPedidos.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnPedidos.Name = "btnPedidos";
+            this.btnPedidos.Size = new System.Drawing.Size(365, 63);
+            this.btnPedidos.TabIndex = 3;
+            this.btnPedidos.Text = "Pedidos";
+            this.btnPedidos.UseVisualStyleBackColor = true;
+            this.btnPedidos.Click += new System.EventHandler(this.btnPedidos_Click);
+            // 
+            // btnSobre
+            // 
+            this.btnSobre.Location = new System.Drawing.Point(111, 466);
+            this.btnSobre.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnSobre.Name = "btnSobre";
+            this.btnSobre.Size = new System.Drawing.Size(144, 63);
+            this.btnSobre.TabIndex = 4;
+            this.btnSobre.Text = "Sobre";
+            this.btnSobre.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(327, 466);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(149, 63);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(566, 108);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Seja bem vindo à Let\'s Drive!\r\n\r\nSelecione uma opção:\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FormMenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 36F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnSobre);
+            this.Controls.Add(this.btnPedidos);
+            this.Controls.Add(this.btnCarros);
+            this.Controls.Add(this.btnCliente);
+            this.Font = new System.Drawing.Font("Courier New", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "FormMenuPrincipal";
+            this.Text = "FormMenuPrincipal";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button btnCliente;
+        private Button btnCarros;
+        private Button btnPedidos;
+        private Button btnSobre;
+        private Button btnSair;
+        private Label label1;
+    }
+}
---
+++ TrabalhoFinal_TP1/FormMenuPrincipal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabalhoFinal_TP1
+{
+    public partial class FormMenuPrincipal : Form
+    {
+        public FormMenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPedidos_Click(object sender, EventArgs e)
+        {
+            FormPedidos fp = new();
+            this.Hide();
+            fp.ShowDialog();
+            this.Show();
+        }
+    }
+}
