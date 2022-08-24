c61609dd06ae62f6c8b2599638c92d5c147b3665
Marcus Fabio <fabio63399@gmail.com>
2022-08-24 15:31:03 -0300

    Inclusão Form Sobre

---
+++ TrabalhoFinal_TP1/FormSobre.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace TrabalhoFinal_TP1
+{
+    partial class FormSobre
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(8, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 121);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Projeto de Final de Módulo Técnicas de Programação #1\r\n\r\nLet\'s Drive - Aluguel de" +
+    " Carros\r\n\r\nIntegrantes: \r\nMarcus Pinto\r\nThiago Costa";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 40);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormSobre
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 206);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "FormSobre";
+            this.Text = "FormSobre";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button button1;
+    }
+}
