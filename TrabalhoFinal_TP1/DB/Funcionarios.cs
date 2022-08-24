32b6a6deac0ceffe92aef02e25fe14b9ee4a6de4
Thiago Costa <thiago_291@live.com>
2022-08-24 14:46:24 -0300

    Classe Funcionários

--- TrabalhoFinal_TP1/DB/Funcionarios.cs
+++ TrabalhoFinal_TP1/DB/Funcionarios.cs
@@ -8,5 +8,18 @@ namespace TrabalhoFinal_TP1.DB
 {
     public class Funcionarios
     {
+        Dictionary<int, string> funcionarios = new()
+        {
+            { 1,   "Rafael Rocha Dias" },
+            { 2,   "Larissa Fernandes Pinto" },
+            { 3,   "Amanda Fernandes Almeida" },
+            {4,   "Kai Cavalcante Souza" },
+            {5,   "Eduarda Gomes Santos"},
+            {6,   "Evelyn Sousa Cavalcanti"},
+            {7,   "Douglas Lima Araújo"},
+            {8,   "Pedro Costa Martins"},
+            {9,   "Nicolas Cunha Dias"},
+            {10,  "Danilo Barbosa Cunha"}
+        };
     }
 }
