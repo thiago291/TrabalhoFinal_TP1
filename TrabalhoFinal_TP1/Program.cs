cac5bcee0713ddb8271ae9c94396fe8bb08edf12
Thiago Costa <thiago_291@live.com>
2022-08-24 17:28:58 -0300

    Adição de clientes para teste.

--- TrabalhoFinal_TP1/Program.cs
+++ TrabalhoFinal_TP1/Program.cs
@@ -11,6 +11,8 @@ namespace TrabalhoFinal_TP1
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            DB.InicializaDBs db = new DB.InicializaDBs();
+            db.db();
             Application.Run(new FormMenuPrincipal());
         }
     }
