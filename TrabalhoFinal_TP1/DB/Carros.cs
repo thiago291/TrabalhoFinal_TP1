27be5be44d14f36a6c43c8fd23932b312c432ebd
Thiago Costa <thiago_291@live.com>
2022-08-24 15:20:53 -0300

    Classes feitas.

--- TrabalhoFinal_TP1/DB/Carros.cs
+++ TrabalhoFinal_TP1/DB/Carros.cs
@@ -8,6 +8,17 @@ namespace TrabalhoFinal_TP1.DB
 {
     public class Carros
     {
+        public string Placa { get; set; }
+        public string InfoCarro { get; set; }
+        public double PrecoDiaria { get; set; }
+        public bool Disponibilidade { get; set; }
 
+        public Carros(string placa, string infoCarro, double precoDiaria, bool disponibilidade)
+        {
+            Placa = placa;
+            InfoCarro = infoCarro;
+            PrecoDiaria = precoDiaria;
+            Disponibilidade = disponibilidade;
+        }
     }
 }
