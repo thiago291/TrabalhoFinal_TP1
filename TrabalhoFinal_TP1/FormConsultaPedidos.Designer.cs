6f438c0f8a0be1e515449cb726386376d15912cb
Marcus Fabio <fabio63399@gmail.com>
2022-08-25 14:23:51 -0300

    Código FormConsultaPedido

--- TrabalhoFinal_TP1/FormConsultaPedidos.Designer.cs
+++ TrabalhoFinal_TP1/FormConsultaPedidos.Designer.cs
@@ -49,6 +49,7 @@
             this.btnDelete = new System.Windows.Forms.Button();
             this.btnConsulta = new System.Windows.Forms.Button();
             this.btnCons = new System.Windows.Forms.Button();
+            this.lblAviso = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lblCodPed
@@ -72,22 +73,26 @@
             // 
             // mtbDataRetorno
             // 
-            this.mtbDataRetorno.Location = new System.Drawing.Point(7, 282);
+            this.mtbDataRetorno.Enabled = false;
+            this.mtbDataRetorno.Location = new System.Drawing.Point(7, 317);
             this.mtbDataRetorno.Name = "mtbDataRetorno";
+            this.mtbDataRetorno.ReadOnly = true;
             this.mtbDataRetorno.Size = new System.Drawing.Size(255, 23);
             this.mtbDataRetorno.TabIndex = 27;
             // 
             // mtbDataAluguel
             // 
-            this.mtbDataAluguel.Location = new System.Drawing.Point(7, 233);
+            this.mtbDataAluguel.Enabled = false;
+            this.mtbDataAluguel.Location = new System.Drawing.Point(7, 268);
             this.mtbDataAluguel.Name = "mtbDataAluguel";
+            this.mtbDataAluguel.ReadOnly = true;
             this.mtbDataAluguel.Size = new System.Drawing.Size(255, 23);
             this.mtbDataAluguel.TabIndex = 26;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(7, 308);
+            this.label6.Location = new System.Drawing.Point(7, 343);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(107, 15);
             this.label6.TabIndex = 25;
@@ -96,7 +101,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 264);
+            this.label5.Location = new System.Drawing.Point(6, 299);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(92, 15);
             this.label5.TabIndex = 24;
@@ -105,7 +110,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(7, 215);
+            this.label4.Location = new System.Drawing.Point(7, 250);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(92, 15);
             this.label4.TabIndex = 23;
@@ -114,7 +119,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 171);
+            this.label3.Location = new System.Drawing.Point(6, 206);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(86, 15);
             this.label3.TabIndex = 22;
@@ -123,7 +128,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 127);
+            this.label2.Location = new System.Drawing.Point(7, 162);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(85, 15);
             this.label2.TabIndex = 21;
@@ -132,7 +137,7 @@
             // lblIdAtend
             // 
             this.lblIdAtend.AutoSize = true;
-            this.lblIdAtend.Location = new System.Drawing.Point(7, 83);
+            this.lblIdAtend.Location = new System.Drawing.Point(7, 118);
             this.lblIdAtend.Name = "lblIdAtend";
             this.lblIdAtend.Size = new System.Drawing.Size(94, 15);
             this.lblIdAtend.TabIndex = 20;
@@ -140,29 +145,35 @@
             // 
             // txb_Nº_Diarias
             // 
-            this.txb_Nº_Diarias.Location = new System.Drawing.Point(7, 326);
+            this.txb_Nº_Diarias.Enabled = false;
+            this.txb_Nº_Diarias.Location = new System.Drawing.Point(7, 361);
             this.txb_Nº_Diarias.Name = "txb_Nº_Diarias";
+            this.txb_Nº_Diarias.ReadOnly = true;
             this.txb_Nº_Diarias.Size = new System.Drawing.Size(255, 23);
             this.txb_Nº_Diarias.TabIndex = 19;
             // 
             // txb_CPF
             // 
-            this.txb_CPF.Location = new System.Drawing.Point(7, 189);
+            this.txb_CPF.Enabled = false;
+            this.txb_CPF.Location = new System.Drawing.Point(7, 224);
             this.txb_CPF.Name = "txb_CPF";
+            this.txb_CPF.ReadOnly = true;
             this.txb_CPF.Size = new System.Drawing.Size(255, 23);
             this.txb_CPF.TabIndex = 18;
             // 
             // txb_ID_Atendente
             // 
-            this.txb_ID_Atendente.Location = new System.Drawing.Point(7, 101);
+            this.txb_ID_Atendente.Enabled = false;
+            this.txb_ID_Atendente.Location = new System.Drawing.Point(7, 136);
             this.txb_ID_Atendente.Name = "txb_ID_Atendente";
+            this.txb_ID_Atendente.ReadOnly = true;
             this.txb_ID_Atendente.Size = new System.Drawing.Size(255, 23);
             this.txb_ID_Atendente.TabIndex = 16;
             // 
             // lblValorTotal
             // 
             this.lblValorTotal.AutoSize = true;
-            this.lblValorTotal.Location = new System.Drawing.Point(7, 353);
+            this.lblValorTotal.Location = new System.Drawing.Point(7, 388);
             this.lblValorTotal.Name = "lblValorTotal";
             this.lblValorTotal.Size = new System.Drawing.Size(61, 15);
             this.lblValorTotal.TabIndex = 25;
@@ -170,30 +181,35 @@
             // 
             // txb_Placa_Carro
             // 
-            this.txb_Placa_Carro.Location = new System.Drawing.Point(7, 145);
+            this.txb_Placa_Carro.Enabled = false;
+            this.txb_Placa_Carro.Location = new System.Drawing.Point(7, 180);
             this.txb_Placa_Carro.Name = "txb_Placa_Carro";
+            this.txb_Placa_Carro.ReadOnly = true;
             this.txb_Placa_Carro.Size = new System.Drawing.Size(255, 23);
             this.txb_Placa_Carro.TabIndex = 17;
             // 
             // mtb_Valor_Total
             // 
-            this.mtb_Valor_Total.Location = new System.Drawing.Point(7, 371);
+            this.mtb_Valor_Total.Enabled = false;
+            this.mtb_Valor_Total.Location = new System.Drawing.Point(7, 406);
             this.mtb_Valor_Total.Name = "mtb_Valor_Total";
+            this.mtb_Valor_Total.ReadOnly = true;
             this.mtb_Valor_Total.Size = new System.Drawing.Size(255, 23);
             this.mtb_Valor_Total.TabIndex = 27;
             // 
             // btnNovaConsulta
             // 
-            this.btnNovaConsulta.Location = new System.Drawing.Point(7, 415);
+            this.btnNovaConsulta.Location = new System.Drawing.Point(7, 450);
             this.btnNovaConsulta.Name = "btnNovaConsulta";
             this.btnNovaConsulta.Size = new System.Drawing.Size(122, 22);
             this.btnNovaConsulta.TabIndex = 28;
             this.btnNovaConsulta.Text = "Nova Consulta";
             this.btnNovaConsulta.UseVisualStyleBackColor = true;
+            this.btnNovaConsulta.Click += new System.EventHandler(this.btnNovaConsulta_Click);
             // 
             // btnVoltar
             // 
-            this.btnVoltar.Location = new System.Drawing.Point(75, 443);
+            this.btnVoltar.Location = new System.Drawing.Point(75, 478);
             this.btnVoltar.Name = "btnVoltar";
             this.btnVoltar.Size = new System.Drawing.Size(122, 22);
             this.btnVoltar.TabIndex = 28;
@@ -203,12 +219,13 @@
             // 
             // btnDelete
             // 
-            this.btnDelete.Location = new System.Drawing.Point(140, 415);
+            this.btnDelete.Location = new System.Drawing.Point(140, 450);
             this.btnDelete.Name = "btnDelete";
             this.btnDelete.Size = new System.Drawing.Size(122, 22);
             this.btnDelete.TabIndex = 28;
             this.btnDelete.Text = "Excluir Pedido";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // btnConsulta
             // 
@@ -229,11 +246,19 @@
             this.btnCons.UseVisualStyleBackColor = true;
             this.btnCons.Click += new System.EventHandler(this.btnCons_Click);
             // 
+            // lblAviso
+            // 
+            this.lblAviso.Location = new System.Drawing.Point(7, 82);
+            this.lblAviso.Name = "lblAviso";
+            this.lblAviso.Size = new System.Drawing.Size(122, 36);
+            this.lblAviso.TabIndex = 30;
+            // 
             // FormConsultaPedidos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(274, 475);
+            this.ClientSize = new System.Drawing.Size(274, 515);
+            this.Controls.Add(this.lblAviso);
             this.Controls.Add(this.btnCons);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnVoltar);
@@ -254,7 +279,9 @@
             this.Controls.Add(this.txb_ID_Atendente);
             this.Controls.Add(this.mtbNumPedido);
             this.Controls.Add(this.lblCodPed);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormConsultaPedidos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Consulta Pedidos";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -284,5 +311,6 @@
         private Button btnDelete;
         private Button btnConsulta;
         private Button btnCons;
+        private Label lblAviso;
     }
 }
